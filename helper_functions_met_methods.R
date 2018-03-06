4960dc4b0ec5dbc6f7bd89d0eead37a5bdc70373
Maureen Carey <maureen.carey@mac.com>
2018-03-06 11:43:54 -0500

    resubmission updates
    resubmission to msphere Feb 2018

--- helper_functions_met_methods.R
+++ helper_functions_met_methods.R
@@ -1,36 +1,21 @@
-library(tidyverse); library(reshape2)
-library(pracma); library(grid); library(pwr)
-library(randomForest); library(e1071); library(caret)
+library(tidyverse)
+library(knitr)
+library(reshape2)
+library(pracma)
+library(grid)
+library(randomForest)
+library(vegan)
+library(gridExtra)
+library(RSvgDevice)
+library(extrafont)
 
-base_directory = '/Users/Mo/Desktop/lab/metabolomics/metabolomics_2017_submit' # REPLACE IF DATA SAVED ELSEWHERE
-setwd(base_directory)
-
-# make function to get stats on each metabolite
-multi.fun = function(x) {
-  c(mean = mean(x, na.rm =T), median = median(x, na.rm =T), var = var(x, na.rm =T), sd = sd(x, na.rm =T)) }
+base_directory = "/Users/maureencarey/local_documents/work/metabolomics_methods/data/" # REPLACE IF DATA SAVED ELSEWHERE
 
 # preprocess dataset function to remove undetected mets
 remove_NAs = function(raw) { # raw = raw file, unprocessed
   # if entire row is nas, remove 
   raw_naR = as.matrix(raw[rowSums(is.na(raw))!=(dim(raw)[2]), ]) #dim = 375  17
   return(raw_naR) }
-
-# preprocess dataset function to scale to median, and imput missing values
-scale_to_median = function(raw_naR) { # raw = raw file or output of remove_NAs
-  stats = {}; storage.mode(raw_naR) = "numeric"
-  for (i in 1:nrow(raw_naR)) {
-    (stats = rbind(stats, multi.fun(as.numeric(raw_naR[i,])))) }
-  row.names(stats) = row.names(raw_naR)
-  data_for_scale = as.data.frame(merge(raw_naR,stats, by = 'row.names'))
-  rownames(data_for_scale) = data_for_scale$Row.names
-  data_for_scale = data_for_scale[,2:dim(data_for_scale)[2]]
-  
-  for (i in 1:nrow(data_for_scale)) { # scale with median met abundance
-    for (j in 1:(ncol(data_for_scale)-4)) { # all columns except stats columns
-      data_for_scale[i,j] = 
-        ( data_for_scale[i,j] / data_for_scale$median[i] ) } }
-  data_scaled = data_for_scale[,1:(ncol(data_for_scale)-4)] # remove stats columns
-  return(data_scaled) }
 
 # preprocess dataset function to imput missing values
 imputing_missing = function(data_scaled, scaled) { # data = raw, raw_naR, or data_scaled file, scaled = 1/0 for if scaled or not
@@ -68,8 +53,8 @@ prep_raw = function(raw) {
 
 # Filter sample identifiers by what is in dataset
 prep_sample_identifiers = function(raw) {
-  setwd(base_directory) # open file containing sample measures
-  identifiers = read.csv("sample_data_2017.csv", sep = ",", header = F, 
+  # open file containing sample measures
+  identifiers = read.csv(paste(base_directory,"sample_data_2017.csv",sep = ""), sep = ",", header = F, 
                          na.strings=c('',"NA"), stringsAsFactors = F)
   rownames(identifiers) = identifiers$V1 # add row names
   identifiers$V1 = NULL # remove column that was moved to row names
@@ -125,11 +110,11 @@ remove_characters_from_colnames = function(dataframe) {
 }
 
 custom_theme_pca = theme(
-  plot.title = element_text(colour="black",size=9),
-  axis.text.x = element_text(colour="black",size=6,angle=0,hjust=.5,vjust=.5,face="plain"), 
-  axis.text.y = element_text(colour="black",size=6,angle=0,hjust=1,vjust=0,face="plain"), 
-  axis.title.x = element_text(colour="black",size=9,angle=0,hjust=.5,vjust=0,face="plain"), 
-  axis.title.y = element_text(colour="black",size=9,angle=90,hjust=.5,vjust=.5,face="plain"),
+  plot.title = element_text(colour="black",size=9, family = "Helvetica"),
+  axis.text.x = element_text(colour="black",size=6,angle=0,hjust=.5,vjust=.5,face="plain", family = "Helvetica"), 
+  axis.text.y = element_text(colour="black",size=6,angle=0,hjust=1,vjust=0,face="plain",family = "Helvetica"), 
+  axis.title.x = element_text(colour="black",size=9,angle=0,hjust=.5,vjust=0,face="plain", family = "Helvetica"), 
+  axis.title.y = element_text(colour="black",size=9,angle=90,hjust=.5,vjust=.5,face="plain",family = "Helvetica"),
   panel.background = element_blank(),
   panel.grid.major = element_blank(),
   panel.grid.minor = element_blank(),
@@ -138,22 +123,11 @@ custom_theme_pca = theme(
   panel.spacing.x = unit(2, "lines"),
   legend.title=element_blank())
 
-custom_theme_dist = theme(
-  axis.text.y = element_text(colour="black",size=6), 
-  axis.title.y = element_text(colour="black",size=9,angle=90,hjust=.5,vjust=0,face="plain"),
-  panel.background = element_blank(),
-  panel.grid.major = element_blank(),
-  panel.grid.minor = element_blank(),
-  panel.border = element_rect(fill = "NA", colour = "NA"),
-  axis.line = element_blank(),
-  panel.spacing.x = unit(0.05, "lines"),
-  legend.title=element_blank())
-
 custom_theme2 = theme(
-  axis.text.x = element_text(colour="black",size=6), 
+  axis.text.x = element_text(colour="black",size=6, family = "Helvetica"), 
   axis.text.y = element_blank(), axis.title.y = element_blank(),
   axis.ticks.y = element_blank(), axis.ticks.x = element_blank(),
-  axis.title.x = element_text(colour="black",size=9),
+  axis.title.x = element_text(colour="black",size=9, family = "Helvetica"),
   panel.background = element_blank(),
   panel.grid.major = element_line(colour = "grey"),
   panel.grid.minor = element_blank(),
@@ -163,11 +137,11 @@ custom_theme2 = theme(
   plot.margin = unit(c(5,5,5,5),"mm"))
 
 custom_theme3 = theme(
-  plot.title = element_text(size = 9),
-  axis.text.x = element_text(colour="black",size=6,angle=0,hjust=.5,vjust=.5,face="plain"), 
-  axis.text.y = element_text(colour="black",size=6,angle=0,hjust=1,vjust=0,face="plain"), 
-  axis.title.x = element_text(colour="black",size=9,angle=0,hjust=.5,vjust=0,face="plain"), 
-  axis.title.y = element_text(colour="black",size=9,angle=90,hjust=.5,vjust=.5,face="plain"),
+  plot.title = element_text(size = 9, family = "Helvetica"),
+  axis.text.x = element_text(colour="black",size=6,angle=0,hjust=.5,vjust=.5,face="plain", family = "Helvetica"), 
+  axis.text.y = element_text(colour="black",size=6,angle=0,hjust=1,vjust=0,face="plain", family = "Helvetica"), 
+  axis.title.x = element_text(colour="black",size=9,angle=0,hjust=.5,vjust=0,face="plain", family = "Helvetica"), 
+  axis.title.y = element_text(colour="black",size=9,angle=90,hjust=.5,vjust=.5,face="plain", family = "Helvetica"),
   panel.background = element_blank(),
   panel.grid.major = element_line(colour = "grey"),
   panel.grid.minor = element_blank(),
@@ -175,7 +149,22 @@ custom_theme3 = theme(
   axis.line = element_blank(),
   panel.spacing.x = unit(2, "lines"))
 
-'%notin%' <- function(x,y)!('%in%'(x,y))
+
+custom_theme4 = theme(
+  plot.title = element_text(colour="black",size=10, family = "Helvetica"),
+  axis.text.x = element_text(colour="black",size=8,angle=0,hjust=.5,vjust=.5,face="plain", family = "Helvetica"), 
+  axis.text.y = element_text(colour="black",size=8,angle=0,hjust=1,vjust=0,face="plain",family = "Helvetica"), 
+  axis.title.x = element_text(colour="black",size=10,angle=0,hjust=.5,vjust=0,face="plain", family = "Helvetica"), 
+  axis.title.y = element_text(colour="black",size=10,angle=90,hjust=.5,vjust=.5,face="plain",family = "Helvetica"),
+  legend.text = = element_text(colour="black",size=8)
+  panel.background = element_blank(),
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank(),
+  panel.border = element_rect(fill = "NA", colour = "black"),
+  axis.line = element_blank(),
+  panel.spacing.x = unit(2, "lines"),
+  legend.title=element_blank(),
+  plot.margin = unit(c(20,2,2,2),"mm"))
 
 median_center_scale_data <- function(x) {
   # x is matrix or dataframe with rows as samples and columns as mets
@@ -188,30 +177,6 @@ median_center_scale_data <- function(x) {
   if (any(is.na(x_centered_scaled))) { warning('sd == 0')}
   return(x_centered_scaled)}
 
-mean_center_scale_data <- function(x) {
-  # x is matrix or dataframe with rows as samples and columns as mets
-  sd_x = apply(x,2,sd,na.rm=TRUE)
-  x_centered = apply(x, 2, function(y) (y - mean(y,na.rm=TRUE)))
-  x_centered_scaled = sweep(x_centered, 2, sd_x, "/")
-  indx = setdiff((which(is.na(x_centered_scaled))),which(is.na(x_centered)))
-  if (any(is.na(x_centered_scaled))) { 
-    x_centered_scaled[indx] = 0}
-  if (any(is.na(x_centered_scaled))) { warning('sd == 0')}
-  return(x_centered_scaled)}
-
-# Get upper triangle of the correlation matrix
-# modified from http://www.sthda.com/english/wiki/
-# ggplot2-quick-correlation-matrix-heatmap-r-software-and-data-visualization
-get_upper_tri <- function(cormat){
-  cormat[lower.tri(cormat)]<- NA
-  return(cormat) }
-
-# also from www.sthda.com
-reorder_cormat <- function(cormat){ # Use correlation between variables as distance
-  dd <- as.dist(cormat); hc <- hclust(dd)
-  cormat <-cormat[hc$order, hc$order] }
-
-
 # make df values numeric instead of character or factor
 make_numeric = function(df) { # doesn't really matter what columns or rows are
   indx = sapply(df, is.factor)
@@ -221,3 +186,16 @@ make_numeric = function(df) { # doesn't really matter what columns or rows are
   return(df) }
 
 rM = base::rowMeans
+
+## get percent variation summarized by pca
+get_1st = function(input_pca) {
+  # class(input_pca) = "prcomp"
+  temp = summary(input_pca)
+  temp2 = temp$importance
+  return(round(temp2[2,1]*100, digits = 2))
+}
+get_2nd = function(input_pca) {
+  temp = summary(input_pca)
+  temp2 = temp$importance
+  return(round(temp2[2,2]*100, digits = 2))
+}
